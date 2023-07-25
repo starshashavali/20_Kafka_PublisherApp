8a1c713651670e6a7f3eedf7b6a929b19abffba1
shasha <javashashavali@gmail.com>
2023-07-25 09:47:37 +0530

    Kafka Publisher app

---
+++ src/main/java/com/tcs/rest/OrderRestController.java
@@ -0,0 +1,26 @@
+package com.tcs.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tcs.binding.Order;
+import com.tcs.service.OrderService;
+
+@RestController
+public class OrderRestController {
+
+	@Autowired
+	private OrderService service;
+
+	@PostMapping("/order")
+	public String createOrder(@RequestBody Order order) {
+		String msg = service.addMsg(order);
+		return msg;
+	}
+
+}
+
+
+
